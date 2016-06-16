0d479ebd8a7642307a734550b2866c2462e33ebf
eddrial <eddrial@hotmail.com>
2016-06-16 16:23:45 +0100

    Correcting setup?

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-      name="src",
+      name="ID_Analyses",
       author = "Ed Rial",
       version = "0.1",
       packages = find_packages(),
       install_requires = ['numpy','h5py'],
       )
+#
