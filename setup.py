e3933be0d041aaeefd19ade1f98de84580277d39
eddrial <eddrial@hotmail.com>
2016-06-16 16:19:14 +0100

    Correcting setup.py?

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-      name="ID_Analyses",
+      name="src",
       author = "Ed Rial",
       version = "0.1",
       packages = find_packages(),
