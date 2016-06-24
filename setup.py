a30de4ddb3d2bb165308236710056b31591852a7
eddrial <eddrial@hotmail.com>
2016-06-24 13:33:00 +0100

    travis and setup rescued by Mark

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-      name="ID_Analyses",
+      name="id_analyses",
       author = "Ed Rial",
       version = "0.1",
       packages = find_packages(),
