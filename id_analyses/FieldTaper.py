a6fe1923e40e3a9bc6b43408fbae1d1803ddce4b
eddrial <eddrial@hotmail.com>
2016-06-27 16:20:26 +0100

    Simplify to basic pretty empty structure

--- id_analyses/FieldTaper.py
+++ id_analyses/FieldTaper.py
@@ -4,47 +4,9 @@ Created on 16 Jun 2016
 @author: gdy32713
 '''
 
-import h5py
-import numpy as np
-import matplotlib.pyplot as plt
+#import h5py
+#import numpy as np
+#import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    f=h5py.File('..\Data\CPMUplota1ideal.h5','r')
-        
-        
-    s = np.array(f['s'])
-    Bxfield = np.array(f['Bx'])
-    Bsfield = np.array(f['Bs'])    
-    Bzfield = np.array(f['Bz'])
-    
-    Bfield = np.array([Bxfield, Bsfield, Bzfield])
-    print (np.shape(Bfield))
-
-    plt.figure(1)
-    line1 = plt.plot(Bzfield)
-    plt.show(1)
-    
-    insx = Bxfield[390:410]
-    inss = Bsfield[390:410]
-    insz = Bzfield[390:410]
-    
-    for x in range(98):
-        Bxfield = np.insert(Bxfield, 410, insx)
-        Bsfield = np.insert(Bsfield, 410, inss)
-        Bzfield = np.insert(Bzfield, 410, insz)
-    
-    
-    Bfield = np.array([Bxfield, Bsfield, Bzfield])
-    print (np.shape(Bfield))
-    
-    plt.figure(2)
-    line2 = plt.plot(np.transpose(Bfield[0:2,:]))
-    plt.show(2)
-    
-    print len(Bzfield)
-    
-    step = s[1]-s[0]
-    L= len(Bzfield)
-    s = np.arange(((-L+1)*step/2),(L*step/2),step)
-    print(s[1380:1382])
-    print len(s)
+    pass
--- id_analyses/loadbfield.py
+++ id_analyses/loadbfield.py
@@ -3,13 +3,13 @@ Created on 16 Jun 2016
 
 @author: gdy32713
 '''
-import h5py
+#import h5py
 
 def loadBFieldFromHDF5():
-    f=h5py.File('..\Data\CPMUplota1ideal.h5','r')
-    print f.ref
+    #f=h5py.File('..\Data\CPMUplota1ideal.h5','r')
+    print 'Just trying to make Travis Work!' #f.ref
     
-    return f
+    #return f
 
 if __name__ == '__main__':
     loadBFieldFromHDF5()
