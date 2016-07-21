b552783dadfb3d2f92547f09c9b79b843b147ef4
eddrial <eddrial@hotmail.com>
2016-07-21 10:47:53 +0100

    use h5py import, try and import hdf5 file

--- id_analyses/loadbfield.py
+++ id_analyses/loadbfield.py
@@ -3,12 +3,12 @@ Created on 16 Jun 2016
 
 @author: gdy32713
 '''
-#import h5py
+import h5py
 
 def loadBFieldFromHDF5():
-    #f=h5py.File('..\Data\CPMUplota1ideal.h5','r')
-    pass
-    #return f
+    f=h5py.File('..\Data\CPMUplota1ideal.h5','r')
+    
+    return f
 
 if __name__ == '__main__':
     a = loadBFieldFromHDF5()
