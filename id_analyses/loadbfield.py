e931dc8ef7bb0cad5004991f0c3d48d75d0d11cc
eddrial <eddrial@hotmail.com>
2016-07-21 13:42:14 +0100

    Changed Data to data filename.

--- id_analyses/loadbfield.py
+++ id_analyses/loadbfield.py
@@ -10,7 +10,7 @@ import os
 def loadBFieldFromHDF5(name):
     path = inspect.stack()[0][1]
     filepath =  '/'.join(os.path.split(path)[0].split(os.sep)[:-2] +
-                    ['Data', name])
+                    ['id_tools/data', name])
     
     f=h5py.File(filepath,'r')
     
