b7affa241a8161f37bb37108d8c065e504611d70
eddrial <eddrial@hotmail.com>
2016-07-21 13:49:50 +0100

    Trying to find test file

--- id_analyses/loadbfield.py
+++ id_analyses/loadbfield.py
@@ -9,8 +9,8 @@ import os
 
 def loadBFieldFromHDF5(name):
     path = inspect.stack()[0][1]
-    filepath =  '/'.join(os.path.split(path)[0].split(os.sep)[:-2] +
-                    ['id_tools/data', name])
+    filepath =  '/'.join(os.path.split(path)[0].split(os.sep)[:-1] +
+                    ['data', name])
     
     f=h5py.File(filepath,'r')
     
